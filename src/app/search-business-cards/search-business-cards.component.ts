f8e1f28020be704bedcd23e17b07d72efc462181
Nathan Chmielewski <njchimmy@gmail.com>
2019-11-26 01:32:22 -0600

    search by first name and organization

--- src/app/search-business-cards/search-business-cards.component.ts
+++ src/app/search-business-cards/search-business-cards.component.ts
@@ -16,6 +16,7 @@ export class SearchBusinessCardsComponent implements OnInit {
     businessCards: Array<BusinessCard>;
     searchResults: AngularFireList<any[]>;
     term: string;
+    searchType: any;
 
   constructor(private db: AngularFireDatabase,
               private authService: AuthService,
@@ -27,8 +28,16 @@ export class SearchBusinessCardsComponent implements OnInit {
 
     queryList(): void {
         console.log('Searching term: ', this.term);
-        this.businessCardsRef = this.db.list('/users/' + this.authService.userId + '/business-cards',
-        ref => ref.orderByChild('firstName').equalTo(this.term)).valueChanges();
+        if(this.searchType === 1) {
+            console.log('Search type: first name');
+            this.businessCardsRef = this.db.list('/users/' + this.authService.userId + '/business-cards',
+            ref => ref.orderByChild('firstName').equalTo(this.term)).valueChanges();
+        }
+        else if(this.searchType === 2) {
+            console.log('Search type: organization');
+            this.businessCardsRef = this.db.list('/users/' + this.authService.userId + '/business-cards',
+            ref => ref.orderByChild('organization').equalTo(this.term)).valueChanges();
+        }
 
     }
 
